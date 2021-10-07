ef9d4d3dcfc3dc542e9f284f5670bec2fc25baac
AleksiVirkkala <aleksi.virkkala@gmail.com>
2021-10-07 17:48:45 +0300

    fix: :lipstick: indentation

--- main.py
+++ main.py
@@ -9,35 +9,36 @@ BOT_ID=open('SECRET.txt', 'r').readline().rstrip('\n')
 
 # Handling message from Telegram
 def handleMessage(msg):
-     id = msg['chat']['id'];
-     command = msg['text'];
-     print ('Command ' + command + ' from chat id' + str(id));
-     if (command == '/photo'):
-       print ("Taking picture…");
-       # Initialize the camera
-       camera = PiCamera();
-       camera.start_preview()
-       camera.rotation = -90
-       camera.capture(path + '/pic.jpg',resize=(600,480))
-       time.sleep(2)
-       camera.stop_preview()
-       camera.close()
-       # Seding picture
-       bot.sendPhoto(id, open(path + '/pic.jpg', 'rb'))
-       bot.sendMessage(id, "MOROOO")
-     elif (command == '/video'):
-       print("Taking video...");
-       camera = PiCamera()
-       camera.resolution = (640, 480)
-       camera.start_recording(path + '/spinni_video.h264')
-       camera.wait_recording(6)
-       camera.stop_recording()
-       camera.close()
-       # Send video
-       bot.sendVideo(id, open(path + '/spinni_video.h264', 'rb'))
-       bot.sendMessage(id, 'YOOOO')
-     else:
-       bot.sendMessage(id, "Laita /photo perkele")
+  id = msg['chat']['id'];
+  command = msg['text'];
+  print ('Command ' + command + ' from chat id' + str(id));
+  if (command == '/photo'):
+    print ("Taking picture…");
+    # Initialize the camera
+    camera = PiCamera();
+    camera.start_preview()
+    camera.rotation = -90
+    camera.capture(path + '/pic.jpg',resize=(600,480))
+    time.sleep(2)
+    camera.stop_preview()
+    camera.close()
+    # Seding picture
+    bot.sendPhoto(id, open(path + '/pic.jpg', 'rb'))
+    bot.sendMessage(id, "MOROOO")
+  elif (command == '/video'):
+    print("Taking video...");
+    camera = PiCamera()
+    camera.resolution = (640, 480)
+    camera.start_recording(path + '/spinni_video.h264')
+    camera.wait_recording(6)
+    camera.stop_recording()
+    camera.close()
+    # Send video
+    bot.sendVideo(id, open(path + '/spinni_video.h264', 'rb'))
+    bot.sendMessage(id, 'YOOOO')
+  else:
+    bot.sendMessage(id, "Laita /photo perkele")
+  
 
 
 
