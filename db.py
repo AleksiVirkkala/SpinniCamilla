32b9954a89a4a2fc2a8c9dd3247c2810abcce5f1
AleksiVirkkala <aleksi.virkkala@gmail.com>
2022-02-17 17:35:58 +0200

    feat: :sparkles: comment out

--- db.py
+++ db.py
@@ -10,4 +10,4 @@ def getDbContent():
 def addToDb(chatID):
   conn = sqlite3.connect('/home/pi/camillatesti/camilla')
   cur = conn.cursor()
-  return cur.execute('SELECT *FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = N\'camilladb\'')
+  # return cur.execute('SELECT *FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = N\'camilladb\'')
