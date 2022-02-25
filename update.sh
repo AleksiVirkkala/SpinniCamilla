7906b7246dcd81d8d6cde23677293cb52b17d721
AleksiVirkkala <aleksi.virkkala@gmail.com>
2022-02-25 14:48:31 +0200

    fix: :loud_sound: fix logging

--- update.sh
+++ update.sh
@@ -2,5 +2,14 @@
 kill $(ps aux | grep '[p]ython' | awk '{print $2}')
 # Get latest code
 git pull
+
+# Add separation and date to different runs
+echo "" >> ./log.txt
+echo "----------" >> ./log.txt
+date >> ./log.txt
+echo "----------" >> ./log.txt
+echo "" >> ./log.txt
+
+
 # Start the bot
-python3 main.py >> ./log.txt &
+python3 -u main.py >> ./log.txt &
