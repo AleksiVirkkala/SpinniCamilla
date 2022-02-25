317c7b8b90021c674fdbb9bfd533a25f9802250f
AleksiVirkkala <aleksi.virkkala@gmail.com>
2022-02-25 15:32:11 +0200

    feat: :sparkles: get logs command

--- admin.py
+++ admin.py
@@ -1,4 +1,12 @@
 def handleMessage(id, command, bot, db):
+  # Get list of user ids that are currently logged in
   if (command == '/db'):
     bot.sendMessage(id, 'Reading database...')
     bot.sendMessage(id, db.getDbContent())
+  
+  # Get log file content
+  if (command == '/log'):
+    bot.sendMessage(id, 'Reading log file...')
+    log = open('log.txt', 'r').readlines()
+    log = "".join(log)
+    bot.sendMessage(id, log)
