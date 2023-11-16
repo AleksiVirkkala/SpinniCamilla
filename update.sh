b9bcec64ccb0daa19ca821cdcc55cd04cafc9674
AleksiVirkkala <aleksi.virkkala@gmail.com>
2023-11-16 13:05:47 +0200

    Improve update script

--- update.sh
+++ update.sh
@@ -1,7 +1,13 @@
+#!/bin/bash
+
+cd /home/pi/FTP/files/SpinniCamilla
+
 # Kill running bot
 kill $(ps aux | grep '[p]ython' | awk '{print $2}')
 # Get latest code
 git pull
+
+sleep 5
 
 # Add separation and date to different runs
 echo "" >> ./log.txt
@@ -12,4 +18,4 @@ echo "" >> ./log.txt
 
 
 # Start the bot
-python3 -u main.py >> ./log.txt &
+python3 -u main.py >> ./log.txt 2>&1
